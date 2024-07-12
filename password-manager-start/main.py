6bb70278589e077c9b502995d75ce94e5bb10771
Pedrorero <79600821+Pedrorero@users.noreply.github.com>
2024-07-12 13:12:44 -0600

    Add files via upload

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -26,19 +28,55 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website:{
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops!", message="Please make sure you haven't left any fields empty")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
-                                                          f"\nWebsite: {website} \nEmail: {email} "
-                                                          f"\nPassword: {password} \nIs it ok to save?")
-    if is_ok:
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
+        try:
+        # is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
+        #                                                   f"\nWebsite: {website} \nEmail: {email} "
+        #                                                   f"\nPassword: {password} \nIs it ok to save?")
+   # if is_ok:
+            with open("data.json", "r") as data_file:
+
+                #READ OLD DATA
+                data = json.load(data_file)
+                #UPDATE OLD DATA WITH NEW DATA
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                #SAVE UPDATED DATA
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+            if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for {website} exist.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -52,34 +90,37 @@ canvas.grid(column=1, row=0)
 
 
 #labels
-website_label = Label(text="Website:")
+website_label = Label(text="Website: ")
 website_label.grid(column=0, row=1)
 
-email_username_label = Label(text="Email/Username:")
+email_username_label = Label(text="Email: ")
 email_username_label.grid(column=0, row=2)
 
-password_label = Label(text="Password:")
+password_label = Label(text="Password: ")
 password_label.grid(column=0, row=3)
 
 
 #buttons
-add_button = Button(text="Add", width=36, highlightthickness=0, command=save_password)
+add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
 
-generate_button = Button(text="Generate Password", highlightthickness=0, command=generate_password)
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
+
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(column=2, row=1)
 
 
 #entries
-website_entry = Entry(width= 36)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
-email_entry = Entry(width= 36)
+email_entry = Entry(width=36)
 email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0, "pedroreynoso80@gmail.com")
 
-password_entry = Entry(width= 21)
+password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3, columnspan=1)
 
 
